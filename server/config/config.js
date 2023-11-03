fa0c30823da3df0ef05fc1ee718c3b784b5e5829
CHUCHUDACHUCHU <yoon7548@gmail.com>
2023-11-03 20:29:25 +0900

    dotenv 설정

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
+require('dotenv').config()
+const env = process.env
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'daew09070515',
-    database: 'flow_schema',
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    user: env.DB_USER,
+    password: env.DB_PWD,
+    database: env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
