cfbdaadc112ae61e90d66f5f83e7ec599eabc6e8
CHUCHUDACHUCHU <yoon7548@gmail.com>
2023-11-05 12:45:10 +0900

    build modifying

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
